d1bcce85e036a9264ae1ef769f79e608502eea81
nac <chotpisit080916c@gmail.com>
2022-09-13 17:27:12 +0700

    copy pasted

---
+++ typescript/src/app.ts
@@ -0,0 +1,40 @@
+interface Course {
+    courseNo: string;
+    abbrName: string;
+    courseNameTh: string;
+    courseNameEn: string;
+    department: string;
+    credit: number;
+    creditHours: string;
+    genEdType: string;
+    updatedAt: string;
+  }
+
+async function getData() {
+    const response = await fetch("https://firstact-api.thinc.in.th/courses");
+    const data = await response.json();
+    const courses: Course[] = data.courses;
+    const container = document.getElementsByName("section")
+    for(const course in courses) {
+      const section = document.createElement("section");
+      section.classList.add("course");
+      section.innerHTML = '<h3> 2110101 COMP PROG</h3> <h4> จำนวนหน่วยกิต</h4> <p> 3 หน่วยกิต</p> <h4>วันที่เรียน</h4> <ul> 
+          <li> วันพุธ</li>
+          <li> วันพฤหัสบดี</li>
+      </ul>
+
+      <h4> จำนวนที่นั่ง</h4>
+      <p> 284/300</p>';
+      const container =document.getElementById("section");
+      container?.appendChild(section);
+    }
+}
+
+function removeAlert() {
+  const alert = document.getElementById("demo");
+  if(alert) {
+    alert.style.display = "none";
+  }
+}
+
+console.log(getData());
